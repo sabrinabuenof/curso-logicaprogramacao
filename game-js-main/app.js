2f381b2b53e3dfd0385a524f0a74963c6d45b082
Sabrina <bina_fbueno@hotmail.com>
2024-01-19 20:22:40 -0300

    criando variaveis

--- game-js-main/app.js
+++ game-js-main/app.js
@@ -1,1 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número ente 1 e 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai, vc descobriu o numero secreto (5)');
+}
