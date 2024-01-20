a6def14ccad4b7cf4585477349bff59b24e4041f
Sabrina <bina_fbueno@hotmail.com>
2024-01-20 10:43:06 -0300

    contador

--- game-js-main/app.js
+++ game-js-main/app.js
@@ -1,20 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute 
+let chute; 
+let tentativas = 1;
 
 // while significa que enquanto chute não for igual ao número secreto 
 while(chute != numeroSecreto) {
     chute = prompt('Escolha um número ente 1 e 10');
     //se o chute for igual ao numero secreto 
     if (chute == numeroSecreto) {
-        alert(`Isso ai, vc descobriu o numero secreto ${numeroSecreto}` );
+        alert(`Isso ai, vc descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas.` );
     } else {
         if ( chute > numeroSecreto) {
             alert(`O número secreto é menor que o ${chute}`);
         } else {
             alert(`O número secreto é maior que o ${chute}`);
         }
+        //tentativas = tentativas + 1;
+        tentativas++;
         
     }
  
