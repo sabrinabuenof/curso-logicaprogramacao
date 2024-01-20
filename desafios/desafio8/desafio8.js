347e616cd1fc8e1d395bff1771e60c0df84b5fa4
Sabrina <bina_fbueno@hotmail.com>
2024-01-20 11:16:54 -0300

    desafios parte II

---
+++ desafios/desafio8/desafio8.js
@@ -0,0 +1,8 @@
+//Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+let contador = 10;
+
+while (contador <=10) {
+    console.log(contador);
+    contador--;
+}
---
+++ desafios/desafio9/desafio9.js
@@ -0,0 +1,9 @@
+//Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+let numero = prompt("Digite um número para a contagem regressiva:");
+
+
+while(numero >= 0) {
+    console.log(numero);
+    numero--;
+}
