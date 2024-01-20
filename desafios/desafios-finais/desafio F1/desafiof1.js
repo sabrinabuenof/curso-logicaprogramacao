b316bdceed15b7cf83c8ba01f2caee80dbbd9361
Sabrina <bina_fbueno@hotmail.com>
2024-01-20 12:34:09 -0300

    alterando o número maximo

---
+++ desafios/desafios-finais/desafio F1/desafiof1.js
@@ -0,0 +1,4 @@
+// Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+
+let linguagemPrefereida = prompt("Qual a linguagem de programação que vc mais gosta ?");
+console.log(linguagemPrefereida);
--- game-js-main/app.js
+++ game-js-main/app.js
@@ -1,12 +1,13 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() *100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute; 
 let tentativas = 1;
 
 // while significa que enquanto chute não for igual ao número secreto 
 while(chute != numeroSecreto) {
-    chute = prompt('Escolha um número ente 1 e 100');
+    chute = prompt(`Escolha um número ente 1 e ${numeroMaximo}`);
     //se o chute for igual ao numero secreto 
     if (chute == numeroSecreto) {
         break;        
