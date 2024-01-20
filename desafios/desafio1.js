0d1c8acfbd4c60e0f51482934874adc6f8d49a62
Sabrina <bina_fbueno@hotmail.com>
2024-01-19 21:47:05 -0300

    desafio1

---
+++ desafios/desafio1.js
@@ -0,0 +1,23 @@
+alert("Boas vindas ao nosso site!");
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro)
+
+let nome1 = prompt("Digite o seu nome");
+let idade1 = prompt("Digite sua idade");
+
+if (idade1 >= 18) {
+    alert("Parabéns! Você pode tirar sua habilitação!");
+}else {
+    alert("Vai continuar andando a pé! :D")
+}
+
+
+
+
--- game-js-main/app.js
+++ game-js-main/app.js
@@ -5,3 +5,5 @@ let chute = prompt('Escolha um número ente 1 e 10');
 if (numeroSecreto == chute) {
     console.log('Isso ai, vc descobriu o numero secreto (5)');
 }
+
+
