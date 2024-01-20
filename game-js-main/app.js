06c7268941357d8200452cd792cba50f0f68dfcd
Sabrina <bina_fbueno@hotmail.com>
2024-01-20 10:35:02 -0300

    utilizando o while

--- game-js-main/app.js
+++ game-js-main/app.js
@@ -1,14 +1,25 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 8;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número ente 1 e 10');
+let chute 
 
-//se o chute for igual ao numero secreto 
-if (numeroSecreto == chute) {
-   alert(`Isso ai, vc descobriu o numero secreto ${numeroSecreto}` );
-} else {
-    alert('Você errou!');
+// while significa que enquanto chute não for igual ao número secreto 
+while(chute != numeroSecreto) {
+    chute = prompt('Escolha um número ente 1 e 10');
+    //se o chute for igual ao numero secreto 
+    if (chute == numeroSecreto) {
+        alert(`Isso ai, vc descobriu o numero secreto ${numeroSecreto}` );
+    } else {
+        if ( chute > numeroSecreto) {
+            alert(`O número secreto é menor que o ${chute}`);
+        } else {
+            alert(`O número secreto é maior que o ${chute}`);
+        }
+        
+    }
+ 
 }
+
 
 
 
