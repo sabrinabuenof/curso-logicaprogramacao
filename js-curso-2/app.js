04c6d424a164bf83fdb5f3426c2c2f802329554c
Sabrina <bina_fbueno@hotmail.com>
2024-03-09 09:23:43 -0300

    manipulando textos

---
+++ js-curso-2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrado = document.querySelector('p');
+paragrado.innerHTML = 'Escolha um número entre 1 e 10';
